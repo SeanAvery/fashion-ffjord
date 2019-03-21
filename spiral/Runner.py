158cfdc593fb67a1e7bcd5fbe3e02ef7dd70cf6c
Sean Pollock <seanpollock3344@gmail.com>
2019-03-21 00:04:42 -0400

    matplotlib setup

--- spiral/Runner.py
+++ spiral/Runner.py
@@ -1,11 +1,18 @@
 import torch
 import torch.nn as nn
 import os
+import matplotlib.pyplot as plt
 
 def create_visualizer():
     # create image dir container
     if not os.path.exists('imgs'):
         os.makedirs('imgs')
+    
+    fig = plt.figure(figsize=(12, 4), facecolor='white')
+    ax_traj = fig.add_subplot(131, frameon=False)
+    ax_phase = fig.add_subplot(132, frameon=False)
+    ax_vecfield = fig.add_subplot(133, frameon=False)
+    plt.show(block=False)
 
 class ODE(nn.Module):
     def __init__(self):

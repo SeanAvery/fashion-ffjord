75fd67c6da6988b9205ebbfb397dc8463865674d
Sean Pollock <seanpollock3344@gmail.com>
2019-03-21 00:11:34 -0400

    adding in ode network

--- spiral/Runner.py
+++ spiral/Runner.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import os
 import matplotlib.pyplot as plt
 
@@ -14,7 +15,7 @@ def create_visualizer():
     ax_vecfield = fig.add_subplot(133, frameon=False)
     plt.show(block=False)
 
-class ODE(nn.Module):
+class ODEFunc(nn.Module):
     def __init__(self):
         super(ODEFunc, self).__init__()
 
@@ -22,6 +23,14 @@ class ODE(nn.Module):
             nn.Linear(2, 50),
             nn.Tanh(),
             nn.Linear(50, 2))
+        
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
+
+        def forward(self, t, y):
+            return self.net(y**3)
 
 if __name__ == '__main__':
     i = 0
@@ -30,4 +39,5 @@ if __name__ == '__main__':
     create_visualizer()
 
     # 2. create ode net
-    # func = ODEFunc()
+    func = ODEFunc()
+    
